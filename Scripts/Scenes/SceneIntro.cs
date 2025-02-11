7e56bed966749ee7c57d9b4c32930f2cbccc3c4b
GGemCo <ggemco.master@gmail.com>
2025-02-11 11:24:11 +0900

    인트로 씬 스크립트 추가

---
+++ Scripts/Scenes/SceneIntro.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace GGemCo.Scripts.Scenes
+{
+    public class SceneIntro : MonoBehaviour
+    {
+        public void OnClickGameStart()
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Loading");
+        }
+    }
+}
