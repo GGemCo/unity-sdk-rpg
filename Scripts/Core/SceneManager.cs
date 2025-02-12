9ba67c86557a3831142e9e5382f4ae9e7a9479d3
GGemCo <ggemco.master@gmail.com>
2025-02-12 23:11:45 +0900

    씬 매니저 스크립트 추가

---
+++ Scripts/Core/SceneManager.cs
@@ -0,0 +1,10 @@
+﻿namespace GGemCo.Scripts.Core
+{
+    public abstract class SceneManager
+    {
+        public static void ChangeScene(string sceneName)
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+        }
+    }
+}
