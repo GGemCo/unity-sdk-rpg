84fecd2c3437df816d660f3d359b6f9336d5756f
GGemCo <ggemco.master@gmail.com>
2025-03-11 17:32:43 +0900

    [Tool] 게임 데이터 저장 폴더 열기 메뉴 추가

---
+++ Editor/GGemCoTool/EtcEditor.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace GGemCo.Editor.GGemCoTool
+{
+    /// <summary>
+    /// PlayerPrefs 데이터 관리 툴
+    /// </summary>
+    public class EtcEditor : EditorWindow
+    {
+        [MenuItem("GGemCoTool/게임 데이터 저장 폴더 열기")]
+        private static void OpenGameDataFolder()
+        {
+            string path = Application.persistentDataPath;
+
+            if (Directory.Exists(path))
+            {
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+                Process.Start(new ProcessStartInfo()
+                {
+                    FileName = path,
+                    UseShellExecute = true
+                });
+#elif UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
+                Process.Start("open", path);
+#endif
+            }
+            else
+            {
+                UnityEngine.Debug.LogError($"폴더를 찾을 수 없습니다: {path}");
+            }
+        }
+
+    }
+}
