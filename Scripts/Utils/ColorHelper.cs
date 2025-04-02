91bebdba57d5aba04b0776bcdd45372df12437eb
GGemCo <ggemco.master@gmail.com>
2025-04-02 22:51:56 +0900

    [Utils] 색상 처리 관련 유틸 클레스 추가

---
+++ Scripts/Utils/ColorHelper.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace GGemCo.Scripts.Utils
+{
+    public static class ColorHelper
+    {
+        /// <summary>
+        /// Hex를 Color로 리턴합니다.
+        /// </summary>
+        /// <param name="hex">Hex (#생략 가능)</param>
+        /// <returns></returns>
+        public static Color HexToColor(string hex)
+        {
+            hex = hex.Replace("#", ""); // "#" 문자 제거
+            if (hex.Length != 6)
+            {
+                Debug.LogError("유효하지 않은 Hex 값입니다.");
+                return Color.white;
+            }
+
+            byte r = byte.Parse(hex.Substring(0, 2), System.Globalization.NumberStyles.HexNumber);
+            byte g = byte.Parse(hex.Substring(2, 2), System.Globalization.NumberStyles.HexNumber);
+            byte b = byte.Parse(hex.Substring(4, 2), System.Globalization.NumberStyles.HexNumber);
+
+            return new Color32(r, g, b, 255);
+        }
+    }
+}
